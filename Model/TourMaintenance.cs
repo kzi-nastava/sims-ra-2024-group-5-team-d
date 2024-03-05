60ae1ad90b2f68419882684996b2ffa14fa64228
Milana Dobrić <milanadobric5@gmail.com>
2024-03-05 11:10:47 +0100

    TourModel

---
+++ Model/TourMaintenance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BookingApp.Model
+{
+    public class TourMaintenance
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public int TourId { get; set; }
+        public int AvailableSeats { get; set; }
+
+        public TourMaintenance() { }
+        public TourMaintenance(int id, DateTime startTime, int tourId, int availableSeats)
+        {
+            Id = id;
+            StartTime = startTime;
+            TourId = tourId;
+            AvailableSeats = availableSeats;
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            StartTime = Convert.ToDateTime(values[1]);
+            TourId = Convert.ToInt32(values[2]);
+            AvailableSeats = Convert.ToInt32(values[3]);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), StartTime.ToString(), TourId.ToString(), AvailableSeats.ToString() };
+            return csvValues;
+        }
+
+    }
+}
---
+++ Model/TourReservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BookingApp.Model
+{
+    public class TourReservation
+    {
+        public int TourGuestId { get; set; }
+        public int TourMaintenanceId { get; set; }
+        public TourReservation() { }
+        public TourReservation(int tourGuestId, int tourMaintenanceId)
+        {
+            TourGuestId = tourGuestId;
+            TourMaintenanceId = tourMaintenanceId;
+        }
+        public void FromCSV(string[] values)
+        {
+            TourGuestId = Convert.ToInt32(values[0]);
+            TourMaintenanceId = Convert.ToInt32(values[1]);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { TourGuestId.ToString(), TourMaintenanceId.ToString() };
+            return csvValues;
+        }
+
+    }
+}
