a1da5050251e57c4a288b53eb6a0a9a227ee1f5e
RizovicTanja <tanjarizovic0@gmail.com>
2024-04-12 14:51:18 +0200

    Added model, repository and service for guest request

---
+++ Appl/UseCases/GuestRequestService.cs
@@ -0,0 +1,41 @@
+﻿using BookingApp.Domain.Models;
+using BookingApp.Domain.RepositoryInterfaces;
+using BookingApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Appl.UseCases
+{
+    public class GuestRequestService
+    {
+        private IGuestRequestRepository guestRequestRepository;
+
+        public GuestRequestService()
+        {
+            guestRequestRepository = Injector.CreateInstance<IGuestRequestRepository>();
+        }
+        public List<GuestRequest> GetAll()
+        {
+            return guestRequestRepository.GetAll();
+        }
+        public GuestRequest Save(GuestRequest request)
+        {
+            return guestRequestRepository.Save(request);
+        }
+        public void Delete(GuestRequest request)
+        {
+            guestRequestRepository.Delete(request);
+        }
+        public GuestRequest Update(GuestRequest request)
+        {
+            return guestRequestRepository.Update(request);
+        }
+        public GuestRequest GetById(int id)
+        {
+            return guestRequestRepository.GetById(id);  
+        }
+    }
+}
---
+++ Domain/Models/GuestRequest.cs
@@ -0,0 +1,70 @@
+﻿using BookingApp.Domain.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Domain.Models
+{
+    public enum STATUS
+    {
+        APPROVED,
+        REJECTED,
+        INPROCESS
+    }
+    public class GuestRequest : ISerializable
+    {
+        public int Id { get; set; }
+        public int ReservationId { get; set; }
+        public DateTime NewReservedFrom { get; set; }
+        public DateTime NewReservedTo { get; set; }
+        public string Comment {  get; set; }
+        public STATUS Status { get; set; }
+
+        public GuestRequest() 
+        { 
+
+        }
+        public GuestRequest(int id, int idReservation, DateTime newFrom, DateTime newTo, string comment, STATUS status)
+        {
+            Id = id;
+            ReservationId = idReservation;
+            NewReservedFrom = newFrom;
+            NewReservedTo = newTo;
+            Comment = comment;
+            Status = status;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), ReservationId.ToString(), NewReservedFrom.ToString(), NewReservedTo.ToString(), Comment, Status.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            ReservationId = Convert.ToInt32(values[1]);
+            NewReservedFrom = Convert.ToDateTime(values[2]);
+            NewReservedTo = Convert.ToDateTime(values[3]);
+            Comment = values[4];
+            Status = (STATUS)Enum.Parse(typeof(TYPE), values[5]);
+        }
+        public bool IsApproved()
+        {
+            return Status == STATUS.APPROVED;
+        }
+
+        public bool IsRejected()
+        {
+            return Status == STATUS.REJECTED;
+        }
+
+        public bool IsInProgress()
+        {
+            return Status == STATUS.INPROCESS;
+        }
+
+    }
+}
