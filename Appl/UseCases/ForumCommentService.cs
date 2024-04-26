2a84b4cd3e5064411836fdf71dffd9ebfbdc5b1c
LukaIvanSD <lukaivanisevic10@gmail.com>
2024-04-26 13:45:02 +0200

    Added Model,repository,service and csv for forum comments

---
+++ Appl/UseCases/ForumCommentService.cs
@@ -0,0 +1,71 @@
+﻿using BookingApp.Domain.Models;
+using BookingApp.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Appl.UseCases
+{
+    public class ForumCommentService
+    {
+        private readonly IForumCommentRepository forumCommentRepository;
+        private AccommodationService accommodationService;
+        private UserService userService;
+        public ForumCommentService() 
+        { 
+            userService = new UserService();
+            accommodationService = new AccommodationService();
+            forumCommentRepository = Injector.CreateInstance<IForumCommentRepository>();
+        }
+        public void ReportComment(int commentId)
+        {
+            ForumComment comment = GetById(commentId);
+            comment.NumberOfReports++;
+            Update(comment);
+        }
+        public void CreateOwnerComment(int forumId, int creatorId, string comment,Location location)
+        {
+            if (OwnerHasAccommodation(location,creatorId))
+            {
+                ForumComment forumComment = new ForumComment(forumId, creatorId, comment, 0, DateTime.Now);
+                Save(forumComment);
+            }
+        }
+        private bool OwnerHasAccommodation(Location location,int ownerId)
+        {
+            User owner=userService.GetById(ownerId);
+            List<Accommodation> accommodations = accommodationService.GetByUser(owner);
+            return accommodations.Any(accommodation=>accommodation.Location.Id==location.Id);
+        }
+        public List<ForumComment> GetAll()
+        {
+            return forumCommentRepository.GetAll();
+        }
+        public List<ForumComment> GetByForumId(int forumId)
+        {
+            return forumCommentRepository.GetByForumId(forumId);
+        }
+        public ForumComment GetById(int id)
+        {
+            return forumCommentRepository.GetById(id);
+        }
+        public ForumComment Update(ForumComment forumComment)
+        {
+            return forumCommentRepository.Update(forumComment);
+        }
+        public ForumComment Save(ForumComment forumComment)
+        {
+            return forumCommentRepository.Save(forumComment);
+        }
+        public void Delete(ForumComment forumComment)
+        {
+            forumCommentRepository.Delete(forumComment);
+        }
+        public List<ForumComment> GetByUser(User user)
+        {
+            return forumCommentRepository.GetByUser(user);
+        }
+    }
+}
---
+++ Repositories/ForumCommentRepository.cs
@@ -0,0 +1,83 @@
+﻿using BookingApp.Domain.Models;
+using BookingApp.Domain.RepositoryInterfaces;
+using BookingApp.Domain.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repositories
+{
+    public class ForumCommentRepository:IForumCommentRepository
+    {
+        private const string FilePath = "../../../Resources/Data/forumComments.csv";
+
+        private readonly Serializer<ForumComment> _serializer;
+        private List<ForumComment> forumComments;
+
+        public ForumCommentRepository()
+        {
+            _serializer = new Serializer<ForumComment>();
+            forumComments = _serializer.FromCSV(FilePath);
+        }
+        public List<ForumComment> GetAll()
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            return forumComments;
+        }
+        public List<ForumComment> GetByForumId(int forumId)
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            return forumComments.FindAll(forumComment => forumComment.ForumId == forumId);
+        }
+        public ForumComment GetById(int id)
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            return forumComments.Find(forumComment => forumComment.Id == id);
+        }
+        public ForumComment Update(ForumComment forumComment)
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            ForumComment current = forumComments.Find(fC => fC.Id == forumComment.Id);
+            int index = forumComments.IndexOf(current);
+            forumComments.Remove(current);
+            forumComments.Insert(index, forumComment);
+            _serializer.ToCSV(FilePath, forumComments);
+            return forumComment;
+        }
+
+        public ForumComment Save(ForumComment forumComment)
+        {
+            forumComment.Id = NextId();
+            forumComments = _serializer.FromCSV(FilePath);
+            forumComments.Add(forumComment);
+            _serializer.ToCSV(FilePath, forumComments);
+            return forumComment;
+        }
+
+        public int NextId()
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            if (forumComments.Count < 1)
+            {
+                return 1;
+            }
+            return forumComments.Max(c => c.Id) + 1;
+        }
+
+        public void Delete(ForumComment forumComment)
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            ForumComment founded = forumComments.Find(fC => fC.Id == forumComment.Id);
+            forumComments.Remove(founded);
+            _serializer.ToCSV(FilePath, forumComments);
+        }
+
+        public List<ForumComment> GetByUser(User user)
+        {
+            forumComments = _serializer.FromCSV(FilePath);
+            return forumComments.FindAll(forumComment => forumComment.CreatorId == user.Id);
+        }
+    }
+}
