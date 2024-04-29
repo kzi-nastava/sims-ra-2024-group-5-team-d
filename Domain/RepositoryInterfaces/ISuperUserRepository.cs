df1cc47626359a0c0715db0c83c3d8d9bcb3565c
RizovicTanja <tanjarizovic0@gmail.com>
2024-04-30 00:48:55 +0200

    Added model, repository, interface and service for super user

---
+++ Domain/RepositoryInterfaces/ISuperUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookingApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Domain.RepositoryInterfaces
+{
+    public interface ISuperUserRepository
+    {
+        public List<SuperUser> GetAll();
+        public SuperUser Save(SuperUser superUser);
+        public int NextId();
+        public void Delete(SuperUser user);
+        public SuperUser Update(SuperUser superUser);
+        public SuperUser GetById(int id);
+        public List<SuperUser> GetByUser(User user);
+    }
+}
