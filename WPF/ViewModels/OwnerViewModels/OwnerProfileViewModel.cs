dcb966e33609e4a6377ebcf8dc659d49858d418d
LukaIvanSD <lukaivanisevic10@gmail.com>
2024-05-12 19:49:15 +0200

    Added profile view and viewmodels for profile

---
+++ WPF/ViewModels/OwnerViewModels/OwnerProfileViewModel.cs
@@ -0,0 +1,65 @@
+﻿using BookingApp.Appl.UseCases;
+using BookingApp.Domain.Models;
+using BookingApp.WPF.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BookingApp.WPF.ViewModels.OwnerViewModels
+{
+    class OwnerProfileViewModel
+    {
+        public ICommand ChangeCredentialsCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        private User loggedInUser;
+        public ProfileViewModel ProfileViewModel { get; set; }
+        private AccommodationRatingService accommodationRatingService; 
+        private AccommodationReservationService accommodationReservationService;
+        private UserService userService;
+        NotifierService notifierService;
+        public ProfileAccommodationViewModel MostPopularAccommodation { get; set; }
+        public ProfileAccommodationViewModel BestRatedAccommodation { get; set; }
+        public ProfileAccommodationViewModel MostBusyAccommodation { get; set; }
+        public ProfileCredentialsViewModel ProfileCredentials { get; set; }
+        public OwnerProfileViewModel(User user)
+        {
+            loggedInUser = user;
+            accommodationRatingService = new AccommodationRatingService();
+            accommodationReservationService = new AccommodationReservationService();
+            userService = new UserService();
+            notifierService = new NotifierService();
+            ProfileViewModel = new ProfileViewModel(user,accommodationRatingService.GetAverageRatingForOwner(user),accommodationReservationService.GetNumberOfReservationsForOwner(user));
+            MostPopularAccommodation = new ProfileAccommodationViewModel(accommodationReservationService.GetMostPopularAccommodationForOwner(user));
+            BestRatedAccommodation = new ProfileAccommodationViewModel(accommodationRatingService.GetBestRatedAccommodationForOwner(user));
+            MostBusyAccommodation = new ProfileAccommodationViewModel(accommodationReservationService.GetMostBusyAccommodationForOwner(user));
+            ProfileCredentials = new ProfileCredentialsViewModel(user);
+
+            ChangeCredentialsCommand = new RelayCommand(ChangeCredentials);
+            SaveCommand = new RelayCommand(Save);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+        private void ChangeCredentials()
+        {
+            ProfileCredentials.IsEditing = true;
+            ProfileCredentials.IsVisible = false;
+        }
+        private void Save()
+        {
+            ProfileCredentials.IsEditing = false;
+            ProfileCredentials.IsVisible = true;
+            userService.UpdateCredentials(loggedInUser, ProfileCredentials.Username, ProfileCredentials.Password);
+            notifierService.ShowSuccess("Credentials updated successfully!");
+        }
+        private void Cancel()
+        {
+            ProfileCredentials.Username = loggedInUser.Username;
+            ProfileCredentials.Password = loggedInUser.Password;
+            ProfileCredentials.IsEditing = false;
+            ProfileCredentials.IsVisible = true;
+        }
+    }
+}
---
+++ WPF/ViewModels/OwnerViewModels/ProfileViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BookingApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.WPF.ViewModels.OwnerViewModels
+{
+    class ProfileViewModel
+    { 
+        public string AvatarPath { get; set; }
+        public string FullName { get; set; }
+        public double AverageRating { get; set; }
+        public string UserType { get; set; }
+        public string NumberOfReservations { get; set; }
+        public ProfileViewModel(User user,double averageRating,int numberOfReservations)
+        {
+            AvatarPath = user.AvatarPath;
+            FullName = user.FullName;
+            AverageRating = averageRating;
+            if(user.IsSuper())
+                UserType = "SUPER OWNER";
+            else
+                UserType = "OWNER";
+            if(numberOfReservations>1 || numberOfReservations==0)
+                NumberOfReservations= numberOfReservations.ToString()+" reservations";
+            else
+                NumberOfReservations = numberOfReservations.ToString() + " reservation";
+
+        }
+    }
+}
